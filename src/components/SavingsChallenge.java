92b47e1778f9ca576046a36cb21242ffc5fda303
Angela Cui <99048369+ayc325@users.noreply.github.com>
2025-09-08 18:53:58 -0400

    started savings challenge feature

---
+++ src/components/SavingsChallenge.java
@@ -0,0 +1,13 @@
+package components;
+
+import java.util.Scanner;
+
+public class SavingsChallenge {
+    public static void main(String[] args) {
+        // Your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your total savings goal:");
+        double savingsGoal = scanner.nextDouble();
+        
+    }
+}
